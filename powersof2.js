11d9e155d7ae9b5a02a57bef60db6a9e6b432073
Rosco Verheij <roscoverheij@Roscos-MacBook-Pro.local>
2018-08-21 09:47:39 +1000

    Created a countdown and built an end function

--- powersof2.js
+++ powersof2.js
@@ -2,14 +2,16 @@ const numInput = $("#numInput");
 const currentNumDisplay = $("#currentNumber");
 const previousAnswer = $("#previousAnswer");
 const resultDisplay = $("#resultDisplay");
+let countDownDisp = $("#countDownDisp");
 let scoreDisplay = $("#score");
 
 let targetNum = 2;
 let score = 0;
 let counter = 1;
-
+countDown(60);
 $(document).on("keypress",(e) => {
-    if(e.keyCode === 13) {
+    if(e.keyCode === 13) { 
+        resultDisplay.removeClass("m-fadeOut"); 
         evaluate();
     }
 });
@@ -36,11 +38,10 @@ function updateDisplay() {
 }
 
 function correct() {
-    resultDisplay.toggleClass("m-fadeOut");
     resultDisplay.removeClass("incorrect");
     resultDisplay.text("Correct!");
     resultDisplay.addClass("correct");
-    resultDisplay.toggleClass("m-fadeOut");
+    resultDisplay.addClass("m-fadeOut");
 }
 
 function incorrect() {
@@ -54,4 +55,26 @@ function increaseScore() {
     score += counter;
     counter++;
     scoreDisplay.text(score);
+}
+
+function countDown(start) {
+    count = setInterval(function() {
+        start--;
+        if(start <= 0) {
+            clearInterval(count);
+            countDownDisp.text(start);
+            end();
+        }
+        countDownDisp.text(start);
+    }, 1000);
+}
+
+function end() {
+    const mainContainer = $("#main-container");
+    const scoreMove = $("#scoreMove");
+    mainContainer.fadeOut(1500, "linear");
+    scoreMove.animate({
+        left: 110,
+        top: 180
+    }, 2500);
 }
